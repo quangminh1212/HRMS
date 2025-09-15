8424fd15cc227840d85147f87e91f37559ee0067
Bach Minh Quang <0947556012abc@gmail.com>
2025-09-16 02:15:08 +0700

    clear all

--- hrms/__init__.py
+++
@@ -1,8 +0,0 @@
-# ruff: noqa: F401
-from .config import load_settings
-from .db import Base, engine, SessionLocal
-from .models import (
-    Unit, Position, Person, Education, WorkProcess, Planning,
-    SalaryRank, SalaryStep, SalaryHistory, Allowance, AnnualEvaluation,
-    User, AuditLog,
-)
--- hrms/init_db.py
+++
@@ -1,6 +0,0 @@
-from .db import Base, engine
-from .models import *  # noqa: F401,F403
-
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
